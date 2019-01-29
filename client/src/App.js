2a9118d4bf41d2d302784e52d8069195b6365327
wisdomtooth <cbtsao47@gmail.com>
2019-01-29 14:45:43 -0600

    initialize React app

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import Display from "./components/Display";
+import Dashboard from "./components/Dashboard";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Display />
+          <Dashboard />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/Display.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Display extends Component {
+  state = {};
+  render() {
+    return <div>display component</div>;
+  }
+}
+
+export default Display;
